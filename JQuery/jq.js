9fde15fbb0e9b9d89303b3435c544a9b4d444325
mecolli2 <michael.e.collins92@gmail.com>
2017-06-28 12:13:49 -0500

    Input all previously written JQuery

--- JQuery/jq.js
+++ JQuery/jq.js
@@ -1,1 +1,15 @@
+$(document).ready(function(){
+	
+	$('.click').click(function(e){
+		
+		var linkHref = $(this).attr('href');
+		
+		$('html, body').animate({
+			scrollTop: $(linkHref).offset().top
+		}, 1000);
+		
+		e.preventDefault();
+	});
+	
+});
 
